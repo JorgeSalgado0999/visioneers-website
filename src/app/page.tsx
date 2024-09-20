ce9d30473be5eff2fa529af5bf6e937cfc4fc859
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2024-09-19 23:02:48 -0600

    Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<FullNavBar />
 			{/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> */}
-			<main className="container bg-slate-800"></main>
+			<main className="container bg-slate-800">
+				<h1 className="text-4xl text-white">
+					No enseñamos inglés, empoderamos empresas y personas
+				</h1>
+			</main>
 		</div>
 	);
 }
