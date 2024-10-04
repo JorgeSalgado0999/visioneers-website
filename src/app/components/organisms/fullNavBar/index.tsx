1c1e3adf2ef9c86ba89fccdf5e78d20c1363695f
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2024-10-04 14:36:12 -0600

    navbar responsive

--- src/app/components/organisms/fullNavBar/index.tsx
+++ src/app/components/organisms/fullNavBar/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ContactBar from "../../molecules/contactBar";
 import NavBar from "../../molecules/navBar";
 import styles from "./fullNavBar.module.css";
+import {ResponsiveBar} from "../responsiveBar";
 
 const FullNavBar = () => {
 	return (
-		<div className={styles.fullNavBar}>
-			<ContactBar />
-			<NavBar />
-		</div>
+		<nav className={styles.fullNavBar}>
+			<div className={styles.desktop}>
+				<ContactBar />
+				<NavBar />
+			</div>
+			<div className={styles.mobile}>
+				<ResponsiveBar />
+			</div>
+		</nav>
 	);
 };
 
---
+++ src/app/components/organisms/responsiveBar/index.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React, {useState} from "react";
+import styles from "./responsiveBar.module.css";
+
+import {FaPhone} from "react-icons/fa";
+import {FaWhatsapp} from "react-icons/fa";
+import {GiHamburgerMenu} from "react-icons/gi";
+import {IoClose} from "react-icons/io5";
+import Link from "next/link";
+
+export const ResponsiveBar = () => {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div className={styles.navBar}>
+			<div className={styles.topBar}>
+				<div
+					className={styles.menuButtonContainer}
+					onClick={() => setOpen(!open)}
+				>
+					<GiHamburgerMenu className={styles.menuButton} />
+				</div>
+
+				<div className={styles.rightSide}>
+					<div className={styles.phonesContainer}>
+						<div className={styles.phoneContainer}>
+							<FaPhone className={styles.phoneIcon} />
+							<a href="tel:+13322688620" className={styles.phoneNumber}>
+								1 33 2268 8620
+							</a>
+						</div>
+						<div className={styles.phoneContainer}>
+							<FaPhone className={styles.phoneIcon} />
+							<a href="tel:+13311855150" className={styles.phoneNumber}>
+								1 33 1185 5150
+							</a>
+						</div>
+					</div>
+					<div className={styles.whatsappButtonContainer}>
+						<a
+							href="https://wa.me/5213311855150"
+							target="_blank"
+							className={styles.whatsappButton}
+						>
+							<FaWhatsapp className={styles.whatsIcon} />
+							Contáctanos
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div
+				className={`${styles.hiddenMenu} ${open ? styles.show : styles.hide}`}
+			>
+				<div
+					className={styles.navButtonContainer}
+					onClick={() => setOpen(!open)}
+				>
+					<IoClose className={styles.menuButtonClose} />
+				</div>
+
+				<ul className={styles.navList}>
+					<li className={styles.navItem}>
+						<Link href="/">Inicio</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link href="/mentorias">Acompañamientos personalizados</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link href="/servicios">Servicios</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link href="/nosotros">Nosotros</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link href="/contacto">Contacto</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
